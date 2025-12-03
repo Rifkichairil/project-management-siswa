b05722aec8260f55cd69f68c2d4c6d25a9579d0f
Rifki Chairil <rfkchrl.03@gmail.com>
2025-12-03 10:07:31 +0700

    [user] Access

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,13 +4,14 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 
+use Filament\Models\Contracts\FilamentUser;
 use Filament\Panel;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
@@ -50,6 +51,8 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+
 
     public function canAccessPanel(Panel $panel): bool
     {
